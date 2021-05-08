69822f97249c9178855bde0ce16f2ee91d2e50ba
lksngy <luk.nagy@gmail.com>
2021-05-08 10:12:20 +0200

    building database

--- mongodb.js
+++ mongodb.js
@@ -1,1 +1,20 @@
-// CRUD operations => create, read , update, delete
+// CRUD operations => create, read , update, delete database entries
+
+// mongodb object
+const mongodb = require('mongodb')
+// getting access to the db functions to operate the CRUDs
+const MongoClient = mongodb.MongoClient
+
+// connection URL
+const connectionURL = 'mongodb://127.0.0.1:27017'
+// database to connect to
+const databaseName = 'task-manager'
+
+
+// two arguments passed: 1- to parse url correctly, 2-callback function - to connect to the db - async operation 
+MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to the database')
+    }
+    console.log('Connected')
+})
