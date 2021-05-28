b0ec3245b7e98c8d2f4648db2b6eecb6f4b544d1
lksngy <luk.nagy@gmail.com>
2021-05-28 22:38:08 +0200

    simple promise chaining

--- playground/pch2.js
+++ playground/pch2.js
@@ -2,11 +2,11 @@ require('../src/db/mongoose')
 const User = require('../src/models/user')
 
 
-//ObjectId("609c329c0712b90dba588b49")
+//ObjectId("609c46082b3fab121402822c")
 
-User.findByIdAndUpdate('609c329c0712b90dba588b49', { age: 27}).then((user) => {
+User.findByIdAndDelete('609c46082b3fab121402822c').then((user) => {
     console.log(user)
-    return User.countDocuments({ age: 27})
+    return User.countDocuments({ age: 25})
 }).then((result) => {
     console.log(result)
 }).catch((e) => {
