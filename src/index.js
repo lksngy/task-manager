be9adfb9d4e2638fac3c30e281f9317792a522dc
lksngy <luk.nagy@gmail.com>
2021-05-29 11:39:10 +0200

    all changed from promises to async/await

--- src/index.js
+++ src/index.js
@@ -11,75 +11,123 @@ const port = process.env.PORT || 3000
 //express for parsing JSON for us
 app.use(express.json())
 
+//// here are different routes handling user's action in the web browser
+
 // route for creating and saving new user
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
+
 })
 
 // route handler for fetching multiple users
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+    
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
+    // User.find({}).then((users) => {
+    //     res.send(users)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
-// route handler for tetching one user
-app.get('/users/:id', (req, res) => {
+// route handler for fetching one user
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-
-    User.findById(_id).then((user) => {
+    console.log(req.params)
+    
+    try {
+        const user = await User.findById(_id)
         if (!user) {
             return res.status(404).send()
         }
         res.send(user)
-
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
+    // User.findById(_id).then((user) => {
+    //     if (!user) {
+    //         return res.status(404).send()
+    //     }
+    //     res.send(user)
+
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
 //route for creating and saving new task
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
-        console.log(task)
+    try {
+        await task.save()
         res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
+    } catch (e) {
+        res.status(400).send()
+    }
+
+    // task.save().then(() => {
+    //     console.log(task)
+    //     res.status(201).send(task)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
 })
 
 // route handler for fetching multiple tasks
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+    
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
 // route handler for fetching one task
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
 
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id)
         if (!task) {
             return res.status(404).send()
         }
-
         res.send(task)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //         return res.status(404).send()
+    //     }
+
+    //     res.send(task)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
 // basic server
